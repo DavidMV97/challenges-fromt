93ab9dc386459918e3772b167458934aded0208f
David <gmunoz@seedem.co>
2021-05-17 15:55:31 -0500

    Menu responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+document.addEventListener( 'DOMContentLoaded' , () => {
+    let buttonMenu = document.querySelector('.menu-hamburguer');
+    let menu = document.querySelector('.container-header');
 
+    buttonMenu.addEventListener('click' , (e) => {
+        e.preventDefault();
+        buttonMenu.classList.toggle('open'); 
+        menu.classList.toggle('show-menu');
+    })
+} )
